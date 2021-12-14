39d40aed824f7e70a61734f860947ecb8bc7dd4a
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-12-14 11:55:15 +0530

    Initial commit

---
+++ solution.py
@@ -0,0 +1,31 @@
+import random
+
+songs_list = [
+    'Onra - Tfbs Intro',
+    'Malik Abdul Rahmaan - Mailbox Money [Nipsey Flip]',
+    'Nekolai - No No',
+    'Paal Singh - Huffr',
+    'Middleeast - Devil In You',
+    'Mr. Hong - Roses (Feat. Dvdkm And Christine Kim)',
+    'Cam Matthews - Lykme',
+    'Ercentius - Awake (%Ercentius Footworkout)',
+    'Low Key - The Gloves',
+    'Co$Mo - I Want To Be Loved.',
+]
+
+
+def rand_song():
+    new_list = []
+    while len(songs_list) != 0:
+        input("Enter your choice:")
+        current_song = random.choice(songs_list)
+        new_list.append(current_song)
+        print(current_song)
+        songs_list.remove(current_song)
+        if len(songs_list) == 0:
+            songs_list = new_list[:]
+
+
+
+
+rand_song()
